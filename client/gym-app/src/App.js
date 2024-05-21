e918ab026ecf2cdfe2f83a99581d1dd7f122b18c
kaustubh <kaustubhpatil1211@gmail.com>
2024-05-21 22:45:09 +0530

    base commit

---
+++ client/gym-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+
+       <h1 className=" text-cyan-800 bg-black text-center h-screen w-screen   ">Gym bhai ka App</h1>
+    </div>
+  );
+}
+
+export default App;
