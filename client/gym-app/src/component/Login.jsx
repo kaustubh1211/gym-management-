b707f8e1e9dcf84c218c4f761f884a1fc910324b
kaustubh <kaustubhpatil1211@gmail.com>
2024-06-22 10:26:05 +0530

    Ensure server/.env is in .gitignore

--- client/gym-app/src/component/Login.jsx
+++ client/gym-app/src/component/Login.jsx
@@ -25,7 +25,7 @@ export default function Login( {onLoginSuccess}) {
         console.log(user);
       })
       .catch((error) => {
-        const errorCode = error.code;
+        const errorCode = error.code; 
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
       });
