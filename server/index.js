3a745b2cb1a0f37a34e589cec4818c72b21458d0
kaustubh <kaustubhpatil1211@gmail.com>
2024-06-29 09:52:46 +0530

    p-change

--- server/index.js
+++ server/index.js
@@ -217,5 +217,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
